aaea89b097696c50646e683c533a0893b9ff3328
Shaokai Lin <shaokai_lin@126.com>
2021-03-16 20:37:49 -0700

    finish three timers examples. Update run script to use z3 executable.

--- src/ucl/observer/static/three_timers/run.sh
+++ src/ucl/observer/static/three_timers/run.sh
@@ -1,1 +1,13 @@
-uclid app.ucl ../lib/common3.ucl main.ucl
+#!/bin/bash
+
+echo '*** Setting up smt directory'
+rm -rf ./smt/ && mkdir -p smt
+
+echo '*** Generating SMT files from UCLID5'
+uclid -g "smt/output" app.ucl ../lib/common3.ucl main.ucl
+
+echo '*** Append (get-model) to each file'
+ls smt | xargs -I {} bash -c 'echo "(get-model)" >> smt/{}'
+
+echo '*** Running Z3'
+ls smt | xargs -I {} bash -c 'echo "Checking {}" && z3 ./smt/{}'
